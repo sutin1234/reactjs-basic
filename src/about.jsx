ddf9e55cbde2ebbf3829b6bdb39ba1c15188770f
sutin1234 <tony.sutin1234@gmail.com>
2021-02-19 23:53:13 +0700

    added day1 code

---
+++ src/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function about() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus explicabo necessitatibus cupiditate animi officiis ducimus odit blanditiis eum itaque labore. Animi, ipsum. Perspiciatis vitae modi quas accusamus, quia provident nobis.
+            </p>
+        </div>
+    )
+}
