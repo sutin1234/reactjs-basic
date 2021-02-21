8d7f241c15b2cec36617a64ef812ef7539e52317
sutin1234 <tony.sutin1234@gmail.com>
2021-02-21 11:44:01 +0700

    day3 layouts

--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -3,14 +3,17 @@ import React from 'react'
 export default function Footer() {
     return (
         <footer className="bg-dark py-4 text-white">
-            <div className="row">
-                <div className="col-md-6">
-                    &copy;CopyRight 2020-2021
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        &copy;CopyRight 2020-2021
                 </div>
-                <div className="col-md-6 text-end">
-                    www.thinny-dev.com
+                    <div className="col-md-6 text-end">
+                        www.thinny-dev.com
+                </div>
                 </div>
             </div>
+
         </footer>
     )
 }
