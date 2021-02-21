76edae4d2e4238efbd9a04366853f0995d30db3b
sutin1234 <tony.sutin1234@gmail.com>
2021-02-22 00:14:15 +0700

    added prompt font

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import routes from './routes.js'
+import './styles/custom.css'
 
 ReactDOM.render(
   <React.StrictMode>
